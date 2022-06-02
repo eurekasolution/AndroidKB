193bcd68394d88b72cdda9e6c9bc6c8d64715640
eurekasolution <nadopro@gmail.com>
2022-06-03 08:03:52 +0900

    Android Init

---
+++ HelloAndroid/app/src/main/java/com/kbstar/helloandroid/MainActivity.java
@@ -0,0 +1,26 @@
+package com.kbstar.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // git testdaf
+        webView.loadUrl("https://www.kbstar.com");
+
+    }
+}
