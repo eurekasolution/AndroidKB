19fb597dd583524faac8141b336c108d03d29a46
eurekasolution <nadopro@gmail.com>
2022-06-03 17:21:18 +0900

    2022-06-03 Fin

--- A10Scroll/app/src/main/java/com/kbstar/a10scroll/MainActivity.java
+++ A10Scroll/app/src/main/java/com/kbstar/a10scroll/MainActivity.java
@@ -2,8 +2,10 @@ package com.kbstar.a10scroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
@@ -22,8 +24,32 @@ public class MainActivity extends AppCompatActivity {
         scrollView = findViewById(R.id.scrollView);
         imageView = findViewById(R.id.imageView);
         scrollView.setHorizontalScrollBarEnabled(true);
-        
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.kbstar1);
+
+        int width = bitmap.getIntrinsicWidth();
+        int height = bitmap.getIntrinsicHeight();
+
+        System.out.println("---------------------------------------- " + width + ", " + height  );
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = width;
+        imageView.getLayoutParams().height = height;
 
 
+    }
+
+    public void onButtonClicked(View v)
+    {
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.kbstar2);
+
+        int width = bitmap.getIntrinsicWidth();
+        int height = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = width;
+        imageView.getLayoutParams().height = height;
     }
 }
