e0e1d62f3677353a64c8ccb126d56bd2a30ae1eb
eurekasolution <nadopro@gmail.com>
2022-06-07 15:49:04 +0900

    B06 16:00

--- B05Orientation/app/src/main/java/com/kbstar/b05orientation/MainActivity.java
+++ B05Orientation/app/src/main/java/com/kbstar/b05orientation/MainActivity.java
@@ -3,6 +3,7 @@ package com.kbstar.b05orientation;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation ==Configuration.ORIENTATION_LANDSCAPE)
+        {
+            printToast("방향 : LANDSCAPE !!!");
+        } else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            printToast("방향 : PORTRAIT !!!");
+        }
+    }
+
+    @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("name", name);
@@ -70,3 +84,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,msg,  Toast.LENGTH_LONG).show();
     }
 }
+
+/*
+AndroidManifest.xml에 방향전환시 속성 유지 설정
+ */
