d904359065961f43ca66a1b24097cc237f2eb3d5
eurekasolution <nadopro@gmail.com>
2022-06-08 10:47:47 +0900

    C01 11:00

--- B09Progress/app/src/main/java/com/kbstar/b09progress/MainActivity.java
+++ B09Progress/app/src/main/java/com/kbstar/b09progress/MainActivity.java
@@ -2,7 +2,9 @@ package com.kbstar.b09progress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar progress;
     Button button;
     Button button2;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
         progress = findViewById(R.id.progressBar); // 막대형
         progress.setIndeterminate(false);
         progress.setProgress(70);
+
+        // STEP C-01
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // 다이얼로그형 프로그레스 바
+
+                dialog = new ProgressDialog(MainActivity.this);
+                dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("스피너 테스트 중입니다.");
+                dialog.show();
+            }
+        });
+
+    }
+
+    public void kbClick(View view)
+    {
+        System.out.println("------------------------------ kbClick");
+        if(dialog != null)
+        {
+            dialog.dismiss();
+        }
     }
 }
--- C01Inflate/app/src/main/java/com/kbstar/c01inflate/MenuActivity.java
+++ C01Inflate/app/src/main/java/com/kbstar/c01inflate/MenuActivity.java
@@ -2,13 +2,42 @@ package com.kbstar.c01inflate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private LinearLayout container;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // 버튼, 컨테이너 객체 찾아오기
+        button = findViewById(R.id.button);
+        System.out.println("++++++++++++++++++++++++++++++ button : " + button.getText().toString());
+
+        container = findViewById(R.id.container);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = ( LayoutInflater )getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+                CheckBox checkBox = container.findViewById(R.id.checkBox);
+                checkBox.setTag("Sub1 로딩 완료");
+
+                button = container.findViewById(R.id.button2);
+                System.out.println("======================= button2 : " + button.getText().toString());
+            }
+        });
     }
 }
