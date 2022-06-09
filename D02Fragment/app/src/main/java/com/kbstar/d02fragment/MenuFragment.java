b432c1588c0a64d7be4aaed14af6f7bcd2dba545
eurekasolution <nadopro@gmail.com>
2022-06-09 14:15:53 +0900

    D02 14:15

--- D02Fragment/app/src/main/java/com/kbstar/d02fragment/MenuFragment.java
+++ D02Fragment/app/src/main/java/com/kbstar/d02fragment/MenuFragment.java
@@ -4,14 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class MenuFragment extends Fragment {
+    private Button btnMain;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        //return inflater.inflate(R.layout.fragment_menu, container, false);
+
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);;
+
+        btnMain = view.findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.changeFragment(0);
+            }
+        });
+
+        return view;
+
     }
 }
