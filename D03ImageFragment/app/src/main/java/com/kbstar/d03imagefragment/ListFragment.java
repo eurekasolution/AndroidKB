2364942105f065c2a31991db986b5330235a9e82
eurekasolution <nadopro@gmail.com>
2022-06-09 15:49:22 +0900

    D03 15:50

--- D03ImageFragment/app/src/main/java/com/kbstar/d03imagefragment/ListFragment.java
+++ D03ImageFragment/app/src/main/java/com/kbstar/d03imagefragment/ListFragment.java
@@ -1,15 +1,48 @@
 package com.kbstar.d03imagefragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ListFragment extends Fragment {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    // inner class 만들듯이, inner interface를 만든다.
+    public static interface ImageSelectCallback {
+        public void changeImage(int index);
+    }
+
+    public ImageSelectCallback callback;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if(context instanceof ImageSelectCallback)
+        {
+            callback = (ImageSelectCallback) context;
+        }
+    }
+
+    /*
+        callback 변수 자료형을 ImageSelectCallback으로 선언한 이유
+            선택한것에 따라 다른 Fragment 이미지 변경하려고 한다.
+            액티비티(프레그먼트1, 2 포함)한테 데이터를 전달해야 한다.
+            만약 액티비티에 changeImage(2)메소드를 정의한 후,
+            changeImage(2) 호출
+     */
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,6 +51,34 @@ public class ListFragment extends Fragment {
         //return inflater.inflate(R.layout.fragment_list, container, false);
 
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_list, container, false);
+
+        // Button 3개 생성
+        button1 = view.findViewById(R.id.button1);
+        button2 = view.findViewById(R.id.button2);
+        button3 = view.findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(callback != null)
+                {
+                    callback.changeImage(2);
+                }
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 
         return view;
     }
--- D03ImageFragment/app/src/main/java/com/kbstar/d03imagefragment/MainActivity.java
+++ D03ImageFragment/app/src/main/java/com/kbstar/d03imagefragment/MainActivity.java
@@ -1,14 +1,30 @@
 package com.kbstar.d03imagefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ListFragment.ImageSelectCallback {
+
+    ImageFragment imageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        imageFragment = (ImageFragment) manager.findFragmentById(R.id.imageFragment);
+    }
+
+    @Override
+    public void changeImage(int index) {
+        if(index == 2)
+        {
+            //
+        }
     }
 }
