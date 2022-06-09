39458d98f2fc17b6d8dadcf379ddd2a092dcc554
eurekasolution <nadopro@gmail.com>
2022-06-09 16:37:50 +0900

    D04 16:35

---
+++ D04ImageFragment/app/src/main/java/com/kbstar/d03imagefragment/MainActivity.java
@@ -0,0 +1,45 @@
+package com.kbstar.d03imagefragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    int[] imageResourceList = {R.drawable.dream01, R.drawable.dream02, R.drawable.dream03 };
+    ImageFragment imageFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        imageFragment = (ImageFragment) manager.findFragmentById(R.id.imageFragment);
+    }
+
+
+    
+    public void changeImage(int index) {
+
+        imageFragment.setImage( imageResourceList[index]  );
+        /*
+        if(index == 1)
+        {
+            imageFragment.setImage(R.drawable.dream01);
+        }
+        if(index == 2)
+        {
+            imageFragment.setImage(R.drawable.dream02);
+        }
+        if(index == 3)
+        {
+            imageFragment.setImage(R.drawable.dream03);
+        }
+
+         */
+    }
+}
