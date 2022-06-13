8e46f0b37dc5847093ca9f03144eff9557e881b1
eurekasolution <nadopro@gmail.com>
2022-06-13 17:24:01 +0900

    F05 17:30

--- F05Broadcast/app/src/main/java/com/kbstar/f05broadcast/SmsReceiver.java
+++ F05Broadcast/app/src/main/java/com/kbstar/f05broadcast/SmsReceiver.java
@@ -3,13 +3,32 @@ package com.kbstar.f05broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
+    private static final String TAG = "SMSReceiver";
 
+
+    // 문자메시지를 받으면 자동으로 호출
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d(TAG, "----------------------------- onReceive()");
+
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] messages = parseSmsMessage(bundle);
+
+
     }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle)
+    {
+        //int[] lotto = new int[6];
+        Object[] objs = (Object[] ) bundle.get("pdus");
+        SmsMessage[] msg = new SmsMessage[objs.length];
+
+        return msg;
+    }
+
 }
