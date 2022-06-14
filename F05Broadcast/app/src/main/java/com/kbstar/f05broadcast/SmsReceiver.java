f2daad07d9853600c72f3428fb1b40826d1fc64a
eurekasolution <nadopro@gmail.com>
2022-06-14 10:54:08 +0900

    F05 11:00

--- F05Broadcast/app/src/main/java/com/kbstar/f05broadcast/SmsReceiver.java
+++ F05Broadcast/app/src/main/java/com/kbstar/f05broadcast/SmsReceiver.java
@@ -3,9 +3,12 @@ package com.kbstar.f05broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
+
+import java.util.Date;
 
 public class SmsReceiver extends BroadcastReceiver {
     private static final String TAG = "SMSReceiver";
@@ -19,6 +22,20 @@ public class SmsReceiver extends BroadcastReceiver {
         Bundle bundle = intent.getExtras();
         SmsMessage[] messages = parseSmsMessage(bundle);
 
+        if(messages != null && messages.length>0)
+        {
+            String sender = messages[0].getOriginatingAddress();
+            Log.i(TAG, "------------------------- from : " + sender);
+
+            String msg = messages[0].getMessageBody();
+            Log.i(TAG, "------------------------- msg : " + msg);
+
+            Date time = new Date(messages[0].getTimestampMillis());
+            Log.i(TAG, "------------------------- time : " + time);
+
+        }
+
+
 
     }
 
@@ -27,6 +44,18 @@ public class SmsReceiver extends BroadcastReceiver {
         //int[] lotto = new int[6];
         Object[] objs = (Object[] ) bundle.get("pdus");
         SmsMessage[] msg = new SmsMessage[objs.length];
+
+        int smsCount = objs.length;
+
+        for(int i=0; i<smsCount; i++) {
+            // 단말 OS버전에 따라 사용 메소드가 다름
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                String format = bundle.getString("format");
+                msg[i] = SmsMessage.createFromPdu((byte[]) objs[i], format);
+            } else {
+                msg[i] = SmsMessage.createFromPdu((byte[]) objs[i]);
+            }
+        }
 
         return msg;
     }
