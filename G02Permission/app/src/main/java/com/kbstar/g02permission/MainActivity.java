55731a5d0f1293ecf9ed0ee57e6e533d315dbb3e
eurekasolution <nadopro@gmail.com>
2022-06-14 15:31:52 +0900

    G02 15:30

--- G02Permission/app/src/main/java/com/kbstar/g02permission/MainActivity.java
+++ G02Permission/app/src/main/java/com/kbstar/g02permission/MainActivity.java
@@ -2,10 +2,13 @@ package com.kbstar.g02permission;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         };
 
+        //textView.setText("READ: " + Manifest.permission.READ_EXTERNAL_STORAGE);
         confirmPermission(permission);
 
     }
@@ -39,9 +43,35 @@ public class MainActivity extends AppCompatActivity {
         {
             String currentPermission = permission[i];
 
+            int check = ContextCompat.checkSelfPermission(this, currentPermission);
+
+            if(check == PackageManager.PERMISSION_GRANTED)
+            {
+                textView.append("\n" + currentPermission + " 권한 있음.");
+            }else
+            {
+                textView.append("\n" + currentPermission + " 권한 없음 XXXXX.");
+
+                // 권한이 없으면 다시 물어보기(권한 설명..)
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this, currentPermission))
+                {
+                    textView.append("\n권한 설명이 필요합니다.");
+                }else
+                {
+                    list.add(currentPermission);
+                }
+            }
+
         }
 
+        Log.d("XXXXX", "============== list size = " + list.size());
         // 2. 위험 권한 부여 요청
+        String[] targetArray = new String[list.size()];
+        list.toArray(targetArray);
+
+        if(list.size() >0)
+            ActivityCompat.requestPermissions(this, targetArray, 101);
+
     }
 
 
@@ -53,10 +83,10 @@ public class MainActivity extends AppCompatActivity {
         {
             if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                textView.setText("권한 승인하였습니다.");
+                textView.append("\n권한 승인하였습니다.");
             }else
             {
-                textView.setText("권한 거절하였습니다.");
+                textView.append("\n권한 거절하였습니다.");
             }
         }
     }
